db0a3e7ded16373d3c18f9e38353cbb5004c5e41
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-13 14:53:04 -0300

    Commit inicial do repositório

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*
+ * Arquivo: app.js
+ * Autor: Paulo Alves
+ * Descrição: responsável pela lógica de funcionamento da aplicação
+ * Data: 13/01/2020
+*/
+
+new Vue({
+    el: '#app',
+    data: {
+
+    },
+    computed: {
+        
+    },
+    methods: {
+        
+    },
+    watch: {
+        
+    },
+})
